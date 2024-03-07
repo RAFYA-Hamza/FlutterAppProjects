9bb283ceac1305e019c9c67b127cfaac45b7d07c
RAFYA Hamza <rafyahamza99@gmail.com>
2024-03-07 23:55:19 +0100

    connect shopping app to backend

--- shopping_list/lib/widgets/groceries.dart
+++ shopping_list/lib/widgets/groceries.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
+import 'package:http/http.dart' as http;
 
 class GroceriesScreen extends StatefulWidget {
   const GroceriesScreen({super.key});
